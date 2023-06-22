260673fb9fa85cb661a0a2d7692851b5ec0f8277
MarioBDC <95398288+MarioBDC@users.noreply.github.com>
2023-06-22 00:11:01 -0600

    Add files via upload

---
+++ js/datosPago.js
@@ -0,0 +1,14 @@
+// Obtiene los parámetros de la URL
+var params = new URLSearchParams(window.location.search);
+var producto = params.get("producto");
+var precio = params.get("precio");
+
+// Actualiza los elementos HTML con los datos del pago
+document.getElementById("producto").textContent = producto;
+document.getElementById("precio").textContent = precio;
+
+// Función para mostrar el mensaje de pago exitoso
+function pagoExitoso() {
+  // Abre la ventana de pago exitoso
+  window.open("pagoExitoso.html", "_blank");
+}
