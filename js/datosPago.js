cbc5d0fce784b5bda37742a5725789ce71bfcbed
MarioBDC <95398288+MarioBDC@users.noreply.github.com>
2023-06-23 23:49:00 -0600

    datosPago.js

--- js/datosPago.js
+++ js/datosPago.js
@@ -8,7 +8,9 @@ document.getElementById("producto").textContent = producto;
 document.getElementById("precio").textContent = precio;
 
 // Función para mostrar el mensaje de pago exitoso
-function pagoExitoso() {
-  // Abre la ventana de pago exitoso
+var datosPago = document.getElementById("datosPago");
+// Agregar un evento de click al botón de registro
+datosPago.addEventListener("click", function () {
+  // Redirigir al usuario a la página de registro
   window.location.href = "pagoExitoso.html";
-}
+});
