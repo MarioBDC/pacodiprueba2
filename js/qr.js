a641e5ec22c0f63fb35d402dacaa93bb108ee0d6
MarioBDC <95398288+MarioBDC@users.noreply.github.com>
2023-07-13 12:15:22 -0600

    Update qr.js

--- js/qr.js
+++ js/qr.js
@@ -1,24 +1,31 @@
+//<!-----------BOTÓN PARA IR A VENTANA PRINCIPAL------------>
+var btnNavBefore = document.getElementById("btnNavBefore");// Obtener el botón de registro
+btnNavBefore.addEventListener("click", function () {// Agregar un evento de click al botón de registro
+  window.location.href = "principal.html";// Redirigir al usuario a la página de registro
+});
+
 function generarQR() {
   var motivoProducto = document.getElementById("motivo").value;
   var cantidadDinero = parseFloat(document.getElementById("cantidad").value);
+  var nomUsuario = document.getElementById("NomUsuario").value;
 
   // Verifica si se ha proporcionado un motivo y una cantidad de dinero válidos
-  if (motivoProducto.trim() === "" || isNaN(cantidadDinero) || cantidadDinero <= 0) {
+  if (nomUsuario.trim() === "" || motivoProducto.trim() === "" || isNaN(cantidadDinero) || cantidadDinero <= 0) {
     alert("Por favor, ingresa un motivo válido y una cantidad mayor a cero.");
     return;
   }
 
   // Construye la URL con los datos del producto y el precio
-  var url = "https://mariobdc.github.io/pacodiprueba2/datosPago.html?producto=" + encodeURIComponent(motivoProducto) + "&precio=" + encodeURIComponent(cantidadDinero);
+  var url = "https://mariobdc.github.io/pacodiprueba2/datosPago.html?producto=" + 
+  encodeURIComponent(motivoProducto) + "&precio=" + encodeURIComponent(cantidadDinero) + encodeURIComponent(nomUsuario);
 
   // Genera el código QR utilizando QRCode.js
   var qrcode = new QRCode(document.getElementById("qrcode"), {
     text: url,
-    width: 800,
-    height: 800
+    width: 900,
+    height: 900
   });
 }
-
 
 
 
