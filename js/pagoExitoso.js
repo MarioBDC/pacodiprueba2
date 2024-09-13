d62591c3ffeebd7b137ef2fda036842df3d94586
MarioBDC <95398288+MarioBDC@users.noreply.github.com>
2024-09-13 02:45:06 -0600

    Delete js directory

--- js/pagoExitoso.js
+++
@@ -1,5 +0,0 @@
-// Función para redirigir a la página principal
-function redirigirPrincipal() {
-  // Redirige a la página principal (principal.html)
-  window.location.href = "principal.html";
-}
--- js/pantallaCarga.js
+++
@@ -1,4 +0,0 @@
-// Espera 3 segundos (3000 milisegundos) y redirige a la página deseada
-setTimeout(function () {
-    window.location.href = "pagoExitoso.html";
-}, 3000);
